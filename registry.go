a301ee03128da05489ceaaea6212cb52aeab0e90
Choko <choko@curioswitch.org>
2024-05-13 18:03:55 +0900

    Allow adding more tasks to meta tasks (#3)

---
+++ registry.go
@@ -0,0 +1,24 @@
+package build
+
+import "github.com/goyek/goyek/v2"
+
+var (
+	formatTasks   goyek.Deps
+	generateTasks goyek.Deps
+	lintTasks     goyek.Deps
+)
+
+// RegisterFormatTask adds a task that should be run during the format command.
+func RegisterFormatTask(task *goyek.DefinedTask) {
+	formatTasks = append(formatTasks, task)
+}
+
+// RegisterGenerateTask adds a task that should be run during the generate command.
+func RegisterGenerateTask(task *goyek.DefinedTask) {
+	generateTasks = append(generateTasks, task)
+}
+
+// RegisterLintTask adds a task that should be run during the lint command.
+func RegisterLintTask(task *goyek.DefinedTask) {
+	lintTasks = append(lintTasks, task)
+}
