7158243e8f892bc92de784af2c1ba5f7cfe3901d
Choko <choko@curioswitch.org>
2024-05-31 11:57:25 +0900

    Update prettier

--- versions.go
+++ versions.go
@@ -2,6 +2,6 @@ package build
 
 const (
 	verGolangCILint = "v1.58.1"
-	verGoPrettier   = "e72150eff8c34a7d10d28fcd932649c95903f1a6"
+	verGoPrettier   = "63191cd5a31b24d6b58e125d5310cc520a44e148"
 	verGoYamllint   = "v1.35.1"
 )
