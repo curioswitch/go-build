064352ad00988f8d21a1d87a0e8287f0d19f44b8
Choko <choko@curioswitch.org>
2024-10-09 17:52:07 +0900

    Update tools (#17)

--- versions.go
+++ versions.go
@@ -1,7 +1,7 @@
 package build
 
 const (
-	verGolangCILint = "v1.58.1"
-	verGoPrettier   = "16a91d6604f1e9fa12ffd31e04e1eb6a28ad5afb"
+	verGolangCILint = "v1.61.0"
+	verGoPrettier   = "v1.33.3"
 	verGoYamllint   = "v1.35.1"
 )
