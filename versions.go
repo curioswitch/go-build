62057dfa177ea21a3b8871f192a3fee775a1b5c4
Choko <choko@curioswitch.org>
2024-11-28 14:32:31 +0900

    Add github lint task (#36)

--- versions.go
+++ versions.go
@@ -1,7 +1,9 @@
 package build
 
 const (
+	verActionlint   = "9091329b07ecae896add4546641a65fd43ff8a8e"
 	verGolangCILint = "v1.62.2"
 	verGoPrettier   = "v3.4.1"
+	verGoShellcheck = "v0.10.0"
 	verGoYamllint   = "v1.35.1"
 )
