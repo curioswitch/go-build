559d1eb4209c6c49d1853d5a7f2b4374604fa30f
Choko <choko@curioswitch.org>
2025-09-03 16:38:12 +0900

    Use go run in download to fully download tools

--- registry.go
+++ registry.go
@@ -10,32 +10,34 @@ var (
 	lintTasks     goyek.Deps
 	testTasks     goyek.Deps
 
-	moduleDownloads = map[string]struct{}{}
+	commandDownloads = map[string]struct{}{}
 )
 
-// RegisterFormatTask adds a task that should be run during the format command.
+// RegisterFormatTask adds a task that should be run during the format task.
 func RegisterFormatTask(task *goyek.DefinedTask) {
 	formatTasks = append(formatTasks, task)
 }
 
-// RegisterGenerateTask adds a task that should be run during the generate command.
+// RegisterGenerateTask adds a task that should be run during the generate task.
 func RegisterGenerateTask(task *goyek.DefinedTask) {
 	generateTasks = append(generateTasks, task)
 }
 
-// RegisterLintTask adds a task that should be run during the lint command.
+// RegisterLintTask adds a task that should be run during the lint task.
 func RegisterLintTask(task *goyek.DefinedTask) {
 	lintTasks = append(lintTasks, task)
 }
 
-// RegisterTestTask adds a task that should be run during the test command.
+// RegisterTestTask adds a task that should be run during the test task.
 func RegisterTestTask(task *goyek.DefinedTask) {
 	testTasks = append(testTasks, task)
 }
 
-// RegisterModuleDownloads registers the module to be downloaded by the download command.
-func RegisterModuleDownloads(modules ...string) {
-	for _, module := range modules {
-		moduleDownloads[module] = struct{}{}
+// RegisterCommandDownloads registers the command to be downloaded by the download task.
+// It will be executed as is - to download Go tools, it should be a valid `go run` command
+// that will exit successfully.
+func RegisterCommandDownloads(commands ...string) {
+	for _, module := range commands {
+		commandDownloads[module] = struct{}{}
 	}
 }
